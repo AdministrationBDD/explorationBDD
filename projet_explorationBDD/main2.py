75bdd6b2ee7823532c2c035103d2b607c61e4b5b
Bourguet <jeremy.bourguet@ynov.com>
2023-01-16 16:42:22 +0100

    Création de git

---
+++ projet_explorationBDD/main2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+from prophet import Prophet
+
+df= pd.read_csv("C:\\Users\\Jeremy\\Documents\\Ynov\\python\\projet_explorationBDD\\Bdd.csv")
+df.head()
+m = Prophet()
+m.fit(df)
+futuresVente = m.make_future_dataframe(periods=300)
+futuresVente.tail(10)
